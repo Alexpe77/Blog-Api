869e5e46e7e19d50583c88e1877dc537ec57e776
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-08-08 16:54:03 +0200

    Add post request method + http status code

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,7 +1,8 @@
 <?php
 require_once './models/model.php';
 
-class Controller {
+class Controller
+{
     public function getAllPostsAsJSON()
     {
         $postModel = new Post();
@@ -11,7 +12,34 @@ class Controller {
         echo json_encode($posts);
     }
 
-    public function error404() {
-        return "Erreur 404";
+    public function createPost()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $postData = json_decode(file_get_contents('php://input'), true);
+
+            if (
+                isset($postData['title']) &&
+                isset($postData['body']) &&
+                isset($postData['author'])
+            ) {
+                $postModel = new Post();
+
+                $postModel->createPost(
+                    $postData['title'],
+                    $postData['body'],
+                    $postData['author']
+                );
+                header('Content-Type: application/json');
+                echo json_encode(['message' => 'Post created successfully']);
+            } else {
+                header('Content-Type: application/json');
+                http_response_code(400);
+                echo json_encode(['error' => 'Missing data']);
+            }
+        } else {
+            header('Content-Type: application/json');
+            http_response_code(405);
+            echo json_encode(['error' => 'Unauthorized method']);
+        }
     }
 }
