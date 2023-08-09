b0c073a731caef64cdd8c19d96a42fcc1866744b
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-08-09 10:26:35 +0200

    Add CRUD controllers

--- config/routes.php
+++ config/routes.php
@@ -5,12 +5,12 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once './models/model.php';
 require_once './controllers/controller.php';
 
-// use App\Controllers;
-
 $controller = new Controller();
 
 $router = new Router();
 
 $router->get('/posts', [$controller, 'getAllPostsAsJSON']);
+
+$router->get('/post/:id', [$controller, 'getPostById']); ///////// TODO : FIX THIS :)
 
 $router->run();
--- controllers/controller.php
+++ controllers/controller.php
@@ -42,4 +42,94 @@ class Controller
             echo json_encode(['error' => 'Unauthorized method']);
         }
     }
+
+    public function getPostById($postId)
+    {
+        $postId = (int)$postId;
+
+        if ($postId > 0) {
+            $postModel = new Post();
+
+            $post = $postModel->getPostById($postId);
+
+            if ($post) {
+                header('Content-Type: application/json');
+                echo json_encode($post);
+            } else {
+
+                header('Content-Type: application/json');
+                http_response_code(404);
+                echo json_encode(['error' => 'Post not found']);
+            }
+        } else {
+            header('Content-Type: application/json');
+            http_response_code(400);
+            echo json_encode(['error' => 'ID not valid']);
+        }
+    }
+
+    public function updatePost($postId)
+    {
+
+        if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+            $postData = json_decode(file_get_contents('php://input'), true);
+            if (
+                isset($postData['title']) &&
+                isset($postData['body']) &&
+                isset($postData['author'])
+            ) {
+                $postId = (int)$postId;
+
+                if ($postId > 0) {
+                    $postModel = new Post();
+
+                    $postModel->updatePost(
+                        $postId,
+                        $postData['title'],
+                        $postData['body'],
+                        $postData['author']
+                    );
+
+                    header('Content-Type: application/json');
+                    echo json_encode(['message' => 'Post updated successfully']);
+                } else {
+                    header('Content-Type: application/json');
+                    http_response_code(400);
+                    echo json_encode(['error' => 'ID not valid']);
+                }
+            } else {
+                header('Content-Type: application/json');
+                http_response_code(400);
+                echo json_encode(['error' => 'Missing data']);
+            }
+        } else {
+            header('Content-Type: application/json');
+            http_response_code(405);
+            echo json_encode(['error' => 'Unauthorized method']);
+        }
+    }
+
+    public function deletePost($postId)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+            $postId = (int)$postId;
+
+            if ($postId > 0) {
+                $postModel = new Post();
+
+                $postModel->deletePost($postId);
+
+                header('Content-Type: application/json');
+                echo json_encode(['message' => 'Post deleted successfully']);
+            } else {
+                header('Content-Type: application/json');
+                http_response_code(400);
+                echo json_encode(['error' => 'ID not valid']);
+            }
+        } else {
+            header('Content-Type: application/json');
+            http_response_code(405);
+            echo json_encode(['error' => 'Unauthorized method']);
+        }
+    }
 }
